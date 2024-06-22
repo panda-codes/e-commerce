3ccb7005537c2e5778017d9053a3ffe5435eb14d
panda-codes <dedeomatse@gmail.com>
2024-06-22 17:52:10 +0100

    working on login page css

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignUp'>
+      <div className="loginsignUp-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" />
+          <input type="email" />
+          <input type="password" />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          I agree to the terms and condition policy
+        </div>
+      </div>
     </div>
   )
 }
