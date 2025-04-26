954dc7e4a1eba32ddf78ce40c339e04ce4cdc00d
panda-codes <dedeomatse@gmail.com>
2025-04-26 09:35:29 +0100

    Done with AddProduct component html structure

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ListProduct.css'
+
+const ListProduct = () => {
+  return (
+    <div className='list-product'>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default ListProduct
